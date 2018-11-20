19f28021d8e2659a82de179ef18515c929ec43f5
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2018-11-20 07:19:44 -0800

    Add "remaining" to make pause/resume work

--- Timer/PomodoroTimer.swift
+++ Timer/PomodoroTimer.swift
@@ -5,32 +5,46 @@ class PomodoroTimer {
         case Work = 25.0, ShortBreak = 5.0, LongBreak = 15.0
     }
 
-    private(set) var since: Date
+    private(set) var since: Date?
     private var secondsTillNextStage: Double
     private(set) var stage: Stage
     private(set) var workCount: Int
+    private(set) var remaining: TimeInterval
     
     init() {
         since = Date.init()
         stage = .Work
         secondsTillNextStage = stage.rawValue * 60.0
+        remaining = 0
         workCount = 0
+    }
+    
+    func pause() {
+        self.since = nil
+    }
+    
+    func resume() {
+        self.since = Date.init()
     }
     
     func switchTo(_ stage: Stage, duration: Double) {
         self.stage = stage
         self.secondsTillNextStage = duration
+        self.remaining = stage.rawValue * 60.0
     }
     
     func switchTo(_ stage: Stage) {
         self.stage = stage
         self.since = Date.init()
+        self.remaining = stage.rawValue * 60.0
         self.secondsTillNextStage = stage.rawValue * 60.0
     }
     
     func update(date: Date) -> Stage {
-        let interval = date.timeIntervalSince(since)
-        if (interval > secondsTillNextStage) {
+        self.remaining -= date.timeIntervalSince(since!)
+        self.since = Date.init()
+
+        if (remaining <= 0.0) {
             switch stage {
             case .Work:
                 workCount += 1
@@ -47,8 +61,7 @@ class PomodoroTimer {
         return stage
     }
     
-    func countDownTillNextStage(date: Date) -> Int {
-        let interval = date.timeIntervalSince(since)
-        return Int(ceil(secondsTillNextStage - interval))
+    func countDownTillNextStage(date: Date) -> TimeInterval {
+        return self.remaining
     }
 }
