8ae16590b70e7f8bd76dc1c88c82f5ddd56f97d4
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2018-11-21 00:13:48 -0800

    Don't use build/derived

--- travis.sh
+++ travis.sh
@@ -1,6 +1,7 @@
 xcodebuild clean build test \
     -scheme Interval -only-testing:IntervalTests \
     -enableCodeCoverage YES clean build test
-xcrun llvm-cov report \
-    -instr-profile=$(find $HOME/Library/Developer/Xcode/DerivedData -name 'Coverage.profdata') \
-    build/derived/Build/Products/Debug/Interval.app/Contents/MacOS/Interval
+
+profdata=$(find $HOME/Library/Developer/Xcode/DerivedData -name 'Coverage.profdata')
+bin=$(find $HOME/Library/Developer/Xcode/DerivedData -name 'Interval')
+xcrun llvm-cov report "-instr-profile=$profdata" "$bin"
