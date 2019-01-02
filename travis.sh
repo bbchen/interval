46f76fd876597a2b0444c2c7f5d4ae0fa7400434
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2019-01-02 15:20:24 -0800

    Make a zip file for S3

--- travis.sh
+++ travis.sh
@@ -4,4 +4,11 @@ xcodebuild clean build test \
 
 profdata=$(find $HOME/Library/Developer/Xcode/DerivedData -name 'Coverage.profdata')
 bin=$(find $HOME/Library/Developer/Xcode/DerivedData -name 'Interval')
-xcrun llvm-cov report "-instr-profile=$profdata" "$bin"
+# xcrun llvm-cov report "-instr-profile=$profdata" "$bin"
+
+commit=$(git log --format=%h HEAD -1)
+
+app_dir=$(find $HOME/Library/Developer/Xcode/DerivedData -name 'Interval.app')
+mkdir build
+cp -r $app_dir build/
+(cd build && zip -r Interval-${commit}.zip Interval.app)
